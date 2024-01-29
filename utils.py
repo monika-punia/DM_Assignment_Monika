8ed1b27fd4cfe91e7ba9711f3a519219e0a1bf36
monika-punia <43316249+monika-punia@users.noreply.github.com>
2024-01-29 00:31:49 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-
-def print_question(question_dict):
-    question = question_dict['Question']
-    answer = question_dict['Answer']
-    justify = question_dict['Justify']
-    print(f"{question=}")
