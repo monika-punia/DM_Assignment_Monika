3e7fb699be6eb9abaedf030b68cb7f14586cc2ed
monika-punia <43316249+monika-punia@users.noreply.github.com>
2024-01-29 01:19:22 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,6 +1,6 @@
 def question5_1():
     answer = {
-            'bin1': [1,2,3,4,5],
+            'bin1': [1,2,3,4,5]
             'bin2': [6,7,8],
             'bin3': [9]
     }
@@ -8,7 +8,7 @@ def question5_1():
 
 def question5_2():
     answer = {
-            'bin1': [1,2,3],
+            'bin1': [1,2,3]
             'bin2': [4,5,6],
             'bin3': [7,8,9]
     }
@@ -16,7 +16,7 @@ def question5_2():
 
 def question5_3():
     answer = {
-            'bin1': [1,5,6,7],
+            'bin1': [1,5,6,7]
             'bin2': [2,3,4],
             'bin3': [8,9]
     }
