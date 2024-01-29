b8eb9982e64a4d09562ea12acf8119497358220d
monika-punia <43316249+monika-punia@users.noreply.github.com>
2024-01-29 01:43:10 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,14 +1,14 @@
 def question6_1():
     answer = {
-        'equal_width': ['no change', 9],
-        'equal_frequency': ['no change', 9]
+        'equal_width': ['no Change', 9],
+        'equal_frequency': ['no Change', 9]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['change', 7],
-        'equal_frequency': ['change', 4]
+        'equal_width': ['Change', 7],
+        'equal_frequency': ['Change', 4]
     }
     return answer
 
