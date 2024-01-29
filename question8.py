49522a32237814125a5adbb22110704dafb1d191
monika-punia <43316249+monika-punia@users.noreply.github.com>
2024-01-28 21:17:06 -0500

    Update question8.py

--- question8.py
+++ question8.py
@@ -3,11 +3,11 @@ def question8_1():
     return answer
 
 def question8_2():
-    answer= 'equally similar'
+    answer = 'equally similar'
     return answer
 
 def question8_3():
-    answer= 'equally similar'
+    answer = 'equally similar'
     return answer 
 
 def question8_4():
