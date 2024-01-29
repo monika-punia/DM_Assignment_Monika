b160e4f99c0c9c7ae70ed50821d19a055090eba3
monika-punia <43316249+monika-punia@users.noreply.github.com>
2024-01-28 23:29:32 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,21 +1,17 @@
 def question10_1():
     answer False 
-    # Explanation: Count data can also be affected by noise because it can introduce variability and affect the accuracy of the counts.
     return answer
 
 def question10_2():
     answer True 
-    # Explanation: The correlation coefficient always falls between -1 and 1 because it measures the linear relationship between two variables, where -1 indicates perfect negative correlation, 0 indicates no correlation, and 1 indicates perfect positive correlation.
     return answer
 
 def question10_3():
     answer False 
-    # Explanation: Aggregating daily time series can make it smaller while preserving trends, but sampling may miss crucial information if the chosen samples aren't representative of the entire series.
     return answer
 
 def question10_4():
     answer False 
-    # Explanation: Noise refers to random, small fluctuations in data, while outliers are data points that significantly deviate from the overall pattern. Though outliers can contribute to noise, they are not always the same.
     return answer
 
 def question10_5():
